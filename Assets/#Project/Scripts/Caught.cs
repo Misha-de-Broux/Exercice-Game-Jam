97080e546e004b1a7135c959acfc70d8da83524b
Misha de Broux <misha.debroux@gmail.com>
2024-04-25 20:28:04 +0200

    refined Caught to reload currentLevel

--- Assets/#Project/Scripts/Caught.cs
+++ Assets/#Project/Scripts/Caught.cs
@@ -8,7 +8,7 @@ public class Caught : MonoBehaviour
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Ennemy")){
-            SceneManager.LoadScene("Level 1");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
